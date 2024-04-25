2bf4117d17a12c3530739c07dfee9f05f43a790e
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-04-24 20:30:32 -0600

    Creacion de TipoEmpleadoDAL y CategoriaDAL

---
+++ CapaDatos/CategoriaDAL.cs
@@ -0,0 +1,67 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CategoriaDAL
+    {
+        Contexto _db;
+
+        // Guardar y actualizar Categoria
+        public int Guardar(Categoria categoria, int id = 0, bool esActualizacion = false)
+        {
+            _db = new Contexto();
+
+            int resultado;
+
+            if (esActualizacion)
+            {
+                categoria.CategoriaId = id;
+
+                _db.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+
+                resultado = categoria.CategoriaId;
+            }
+            else
+            {
+                _db.Categorias.Add(categoria);
+                _db.SaveChanges();
+
+                resultado = categoria.CategoriaId;
+            }
+
+            return resultado;
+        }
+
+        // Ver Categoria
+        public List<Categoria> Leer()
+        {
+            _db = new Contexto();
+
+            return _db.Categorias.ToList();
+        }
+
+        // Eliminar Categoria
+        public int Eliminar(int Id)
+        {
+            _db = new Contexto();
+            int resultado;
+
+            var categoria = _db.Categorias.Find(Id);
+            if (categoria != null)
+            {
+                _db.Categorias.Remove(categoria);
+                _db.SaveChanges();
+            }
+            resultado = categoria.CategoriaId;
+            return resultado;
+
+        }
+
+    }
+}
---
+++ CapaDatos/TipoEmpleadoDAL.cs
@@ -0,0 +1,67 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class TipoEmpleadoDAL
+    {
+        Contexto _db;
+
+        // Guardar y actualizar TipoEmpleado
+        public int Guardar(TipoEmpleado tipoEmpleado, int id = 0, bool esActualizacion = false)
+        {
+            _db = new Contexto();
+
+            int resultado;
+
+            if (esActualizacion)
+            {
+                tipoEmpleado.TipoEmpleadoId = id;
+
+                _db.Entry(tipoEmpleado).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+
+                resultado = tipoEmpleado.TipoEmpleadoId;
+            }
+            else
+            {
+                _db.TipoEmpleados.Add(tipoEmpleado);
+                _db.SaveChanges();
+
+                resultado = tipoEmpleado.TipoEmpleadoId;
+            }
+
+            return resultado;
+        }
+
+        // Ver TipoEmpleado
+        public List<TipoEmpleado> Leer()
+        {
+            _db = new Contexto();
+
+            return _db.TipoEmpleados.ToList();
+        }
+
+        // Eliminar TipoEmpleado
+        public int Eliminar(int Id)
+        {
+            _db = new Contexto();
+            int resultado;
+
+            var tipoEmpleado = _db.TipoEmpleados.Find(Id);
+            if (tipoEmpleado != null)
+            {
+                _db.TipoEmpleados.Remove(tipoEmpleado);
+                _db.SaveChanges();
+            }
+            resultado = tipoEmpleado.TipoEmpleadoId;
+            return resultado;
+
+        }
+
+    }
+}
