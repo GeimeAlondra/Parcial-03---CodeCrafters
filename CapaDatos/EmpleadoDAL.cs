041e8fb35a67851a52e32e0759d0db510380654b
Alexander_pineda <126535792+alexander9753100@users.noreply.github.com>
2024-05-15 06:08:04 +0200

    Update EmpleadoDAL.cs

--- CapaDatos/EmpleadoDAL.cs
+++ CapaDatos/EmpleadoDAL.cs
@@ -1,10 +1,4 @@
-﻿using CapaEntidades;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CapaDatos
 {
