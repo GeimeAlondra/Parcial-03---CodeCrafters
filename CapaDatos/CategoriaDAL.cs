19912b9b3e43ce991627c42ded1d46771dba5899
JosueCast <126531292+JosueCast@users.noreply.github.com>
2024-05-14 21:48:23 -0600

    nose

--- CapaDatos/CategoriaDAL.cs
+++ CapaDatos/CategoriaDAL.cs
@@ -12,31 +12,7 @@ namespace CapaDatos
         Contexto _db;
 
         // Guardar y actualizar Categoria
-        public int Guardar(Categoria categoria, int id = 0, bool esActualizacion = false)
-        {
-            _db = new Contexto();
-
-            int resultado;
-
-            if (esActualizacion)
-            {
-                categoria.CategoriaId = id;
-
-                _db.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
-                _db.SaveChanges();
-
-                resultado = categoria.CategoriaId;
-            }
-            else
-            {
-                _db.Categorias.Add(categoria);
-                _db.SaveChanges();
-
-                resultado = categoria.CategoriaId;
-            }
-
-            return resultado;
-        }
+       
 
         // Ver Categoria
         public List<Categoria> Leer()
