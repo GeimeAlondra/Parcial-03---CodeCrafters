b86b4635824a8dbee0b951e4863ad4a3a52a6d47
Elizabeth Abrego <126340374+elizzzAb@users.noreply.github.com>
2024-05-14 22:28:36 -0600

    ModificaciónMarca

--- Capa Logica/MarcasLOG.cs
+++ Capa Logica/MarcasLOG.cs
@@ -1,4 +1,10 @@
-
+﻿using CapaEntidades;
+using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Capa_Logica
 {
