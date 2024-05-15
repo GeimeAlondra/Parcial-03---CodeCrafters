beb65c6062ab9657b21474f91171aaff24e5ac1f
wendy97531234 <126539753+wendy97531234@users.noreply.github.com>
2024-05-15 01:14:20 -0300

    update

--- CapaEntidades/Categoria.cs
+++ CapaEntidades/Categoria.cs
@@ -8,14 +8,5 @@ using System.Threading.Tasks;
 
 namespace CapaEntidades
 {
-    public class Categoria
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int CategoriaId { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string CategoriaNombre { get; set; }
-    }
+   
 }
