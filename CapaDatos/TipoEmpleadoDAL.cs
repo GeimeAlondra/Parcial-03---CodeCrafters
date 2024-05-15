2aac0a07c854928241305b2e1f49527f3ed2d94c
Alexander_pineda <126535792+alexander9753100@users.noreply.github.com>
2024-05-15 06:26:08 +0200

    solución al error en Tipo Empleado

--- CapaDatos/TipoEmpleadoDAL.cs
+++ CapaDatos/TipoEmpleadoDAL.cs
@@ -6,3 +6,71 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+namespace CapaDatos
+{
+    public class TipoEmpleadoDAL
+    {
+        Contexto _db;
+
+        // Guardar y actualizar TipoEmpleado
+        public int Guardar(TipoEmpleado tipoEmpleado, int id = 0, bool esActualizacion = false)
+        {
+            _db = new Contexto();
+
+            int resultado;
+
+            if (esActualizacion)
+            {
+                tipoEmpleado.TipoEmpleadoId = id;
+
+                _db.Entry(tipoEmpleado).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+
+                resultado = tipoEmpleado.TipoEmpleadoId;
+            }
+            else
+            {
+                _db.TipoEmpleados.Add(tipoEmpleado);
+                _db.SaveChanges();
+
+                resultado = tipoEmpleado.TipoEmpleadoId;
+            }
+
+            return resultado;
+        }
+
+        // Ver TipoEmpleado
+        public List<TipoEmpleado> Leer()
+        {
+            _db = new Contexto();
+
+            return _db.TipoEmpleados.ToList();
+        }
+
+        // Eliminar TipoEmpleado
+        public int Eliminar(int Id)
+        {
+            _db = new Contexto();
+            int resultado;
+
+            var tipoEmpleado = _db.TipoEmpleados.Find(Id);
+            if (tipoEmpleado != null)
+            {
+                _db.TipoEmpleados.Remove(tipoEmpleado);
+                _db.SaveChanges();
+            }
+            resultado = tipoEmpleado.TipoEmpleadoId;
+            return resultado;
+
+        }
+
+        //Obtner por Id
+        public TipoEmpleado LeerPorId(int id)
+        {
+            _db = new Contexto();
+
+            return _db.TipoEmpleados.Find(id);
+        }
+
+    }
+}
