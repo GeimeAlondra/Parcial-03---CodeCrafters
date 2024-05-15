3590334d6ff882147417a1c67a680a04de05d5da
Alexander_pineda <126535792+alexander9753100@users.noreply.github.com>
2024-05-15 06:08:58 +0200

    modificando métodos

--- CapaDatos/EmpleadoDAL.cs
+++ CapaDatos/EmpleadoDAL.cs
@@ -1,4 +1,10 @@
-
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CapaDatos
 {
