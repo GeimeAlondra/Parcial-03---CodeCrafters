de3ddef1cf9575b4b35944eadf8d67dc1a44e735
bryan-br0n <126531322+bryan-br0n@users.noreply.github.com>
2024-05-16 12:16:48 -0600

    Creando entidad cliente

--- CapaEntidades/Cliente.cs
+++ CapaEntidades/Cliente.cs
@@ -10,32 +10,6 @@ namespace CapaEntidades
 {
     public class Cliente
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ClienteId { get; set; }
-
-        [Required]
-        [MaxLength(80)]
-        public string ClienteNombre { get; set; }
-
-        [Required]
-        [MaxLength(80)]
-        public string ClienteApellido { get; set; }
-
-
-        [Required]
-        [MaxLength(100)]
-        public string ClienteDireccion { get; set; }
-
-        [Required]
-        [MaxLength(150)]
-        public string ClienteCorreo { get; set; }
-
-
-        [Required]
-        public int ClienteTelefono{ get; set; }
-
-        [Required]
-        public bool Estado { get; set; }
+        
     }
 }
