25dfc17a5ef473cf41230993e9d34ef3e9fcb162
bryan-br0n <126531322+bryan-br0n@users.noreply.github.com>
2024-05-16 12:14:28 -0600

    Finalizando método obtenerproducto

--- Capa Logica/ProductoLOG.cs
+++ Capa Logica/ProductoLOG.cs
@@ -18,7 +18,13 @@ namespace Capa_Logica
 
             return _ProductoDAL.Guardar(producto, id, esActualizacion);
         }
+        
+        public Producto ObtenerProductoPorId(int codigo)
+        {
+            _ProductoDAL = new ProductoDAL();
 
+            return _ProductoDAL.LeerPorId(codigo);
+        }
         
 
         public List<Producto> ObtenerProductos(bool inactivos = false)
